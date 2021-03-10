ec2e4ea3d66a865473e78dbb14447511ca76783a
Greg Pennefather <gregpennefatherdev@gmail.com>
2021-03-10 16:16:09 +0200

    Hex.Monogame extension to TerrainMap

---
+++ src/HexPal.Monogame/TerrainMapExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+
+namespace HexPal.Monogame
+{
+    public static class TerrainMapExtensions
+    {
+        public static Vector2 SizeVector(this TerrainMap terrainMap)
+        {
+            return new Vector2(terrainMap.Size.x, terrainMap.Size.y);
+        }
+
+        public static Vector2 OriginVector(this TerrainMap terrainMap)
+        {
+            return new Vector2(terrainMap.Origin.x, terrainMap.Origin.y);
+        }
+
+        public static Vector2 HexToPositionVector(this TerrainMap terrainMap, Hex hex)
+        {
+            (float x, float y) position = Conversion.HexToPosition(hex, terrainMap.Orientation, terrainMap.Size, terrainMap.Origin);
+            return new Vector2(position.x, position.y);
+        }
+
+        public static Hex PositionVectorToHex(this TerrainMap terrainMap, Vector2 positionVector)
+        {
+            (float x, float y) position = (positionVector.X, positionVector.Y);
+            return Conversion.PositionToHex(position, terrainMap.Orientation, terrainMap.Size, terrainMap.Origin);
+        }
+    }
+}
